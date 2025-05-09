78567d89e09ff56fd03e11c9a70e25e8429b8054
轻尘 <53001305+SlightDust@users.noreply.github.com>
2025-05-09 17:24:31 +0800

    :sparkles: 支持上传加密压缩包

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 ## 安装方法
 1. 在module目录下克隆本仓库 `git clone https://github.com/SlightDust/jmcomic_HoshinoBot.git`
-2. 下载依赖 `pip install jmcomic PyPDF2 img2pdf`  (pyyaml应该都有吧)  
+2. 下载依赖 `pip install jmcomic PyPDF2 img2pdf pyzipper`  (pyyaml应该都有吧)  
 3. 复制`jm_config_example.yml`到`jm_config.yml`
 4. 在`__bot__.py`的`module`中添加`jmcomic_HoshinoBot`
 5. 在群内发送`enable jmcomic`以启用本插件
@@ -18,7 +18,9 @@
 
 ## 更新日志
 2025.05.06 可用  
-2025.05.07 加密PDF文件的输出路径读取`jmcomic_config.yml` by[@NekoYSure](https://github.com/NekoYSure)；本地存在目标jmid文件时直接上传。  
+2025.05.08 加密PDF文件的输出路径读取`jmcomic_config.yml`by[@NekoYSure](https://github.com/NekoYSure)；本地存在目标jmid文件时直接上传。增加依赖`pyyaml`。  
+2025.05.09 可选上传1.未加密pdf、2.加密pdf、3.加密zip+未加密pdf、4.加密zip+加密pdf。增加依赖`pyzipper`。  
+
 
 
 ## 碎碎念
@@ -27,10 +29,13 @@
 也可以看看[Fatfish588/jmid2name-hoshino](https://github.com/Fatfish588/jmid2name-hoshino)，这个是jmid转漫画名字的。  
 
 `jm_config.yml`的第4行是下载的全部图片文件的路径，35行是输出pdf的路径，如果是windows用的话要改成windows的路径格式。这个配置文件的其他设置可以参考[jmcomic配置文件指南](https://jmcomic.readthedocs.io/zh-cn/latest/option_file_syntax/#)。  
+加密PDF、压缩包也都会输出到35行的这个路径  
+
+搞加密zip主要是防止内鬼用手机QQ内置阅读器打开PDF。  
   
 默认情况下，仅允许SUPERUSER权限的用户开启本插件，可以按需求该manage_priv。  
   
-默认情况下，文件密码是jmid的倒序，可以改`_enctypt_pdf`函数的password传参设置别的密码。  
+默认情况下，文件密码是jmid的倒序，可以改`enctypt_pdf`和`encrypt_zip`的password传参设置别的密码。  
   
 ~~`llob_cross_host_download_file`这个我只试了LLOneBot。根据[NapCat文档](https://napneko.github.io/onebot/napcat#napcat-%E8%B5%84%E6%BA%90-url-%E5%8F%82%E6%95%B0%E7%B1%BB%E5%9E%8B)，这么写对于NapCat似乎不太适用。有人用的话试一下吧(x)~~  
 ↑群友说不用改
--- jmcomic_HoshinoBot.py
+++ jmcomic_HoshinoBot.py
@@ -4,6 +4,7 @@ from PyPDF2 import PdfReader, PdfWriter
 import base64
 import os
 import yaml
+import pyzipper
 
 from hoshino import Service, priv
 
@@ -12,17 +13,25 @@ sv = Service('jmcomic', enable_on_default=False, visible=False, use_priv = priv.
 _current_path = os.path.dirname(__file__)
 config_path = os.path.join(_current_path, 'jm_config.yml')
 
-                                 
+UPLOAD_FILE_TYPE = 3
+'''
+1: 未加密PDF（相思了？）
+2: 加密PDF
+3: 加密zip，里面是未加密PDF (recommended)
+4. 加密zip，里面是加密PDF
+'''
 
-#读取config上传目录
+assert UPLOAD_FILE_TYPE in [1, 2, 3, 4], "UPLOAD_FILE_TYPE must be 1, 2, 3 or 4"
+
 def read_pdf_dir_config(config_path):
+    '''从jmcomic配置文件里读取pdf_dir路径'''
     with open(config_path, 'r', encoding='utf-8') as file:
         config = yaml.safe_load(file)
     
     pdf_dir = config.get('plugins', {}).get('after_album', [{}])[0].get('kwargs', {}).get('pdf_dir')
     return pdf_dir
 
-_pdf_path = read_pdf_dir_config(config_path)
+_output_path = read_pdf_dir_config(config_path)
 
 
 def reverse_string(s):
@@ -76,6 +85,23 @@ async def encrypt_pdf(input_pdf_path, output_pdf_path, password):
     '''异步加密PDF文件，返回True表示加密成功，False表示加密失败'''
     return await asyncio.to_thread(_enctypt_pdf, input_pdf_path, output_pdf_path, password)
 
+def _encrypt_zip(input_file_path, output_pdf_path, password):
+    zip = pyzipper.AESZipFile(output_pdf_path, "w", compression=pyzipper.ZIP_DEFLATED, encryption=pyzipper.WZ_AES)  
+    # win10资源管理器不能解压AES加密的压缩文件，得用7zip之类的
+    try:
+        zip.setpassword(password.encode("utf-8"))
+        zip.write(input_file_path, os.path.basename(input_file_path), compress_type=pyzipper.ZIP_DEFLATED)
+        zip.close()
+        return True
+    except Exception as e:
+        zip.close()
+        print(f"Error encrypting zip file: {e}")
+        return False
+
+async def encrypt_zip(input_file_path, output_pdf_path, password):
+    '''异步加密zip文件，返回True表示加密成功，False表示加密失败'''
+    return await asyncio.to_thread(_encrypt_zip, input_file_path, output_pdf_path, password)
+
 async def llob_cross_host_download_file(bot, b64):
     '''用于llonebot将base64编码的文件下载到本地。
     适用于Hoshino和llonebot不在同一主机或使用了docker的情况。
@@ -92,36 +118,84 @@ async def is_exist_comic(comic_id):
     Returns:
       bool: True表示漫画存在，False表示漫画不存在
     '''
-    if os.path.exists(_pdf_path+str(comic_id)+"_encrypted.pdf"):
-        return True
-    else:
-        return False
-    
+    if UPLOAD_FILE_TYPE == 1:
+        return os.path.exists(_output_path+str(comic_id)+".pdf")
+    elif UPLOAD_FILE_TYPE == 2:
+        return os.path.exists(_output_path+str(comic_id)+"_encrypted.pdf")
+    elif UPLOAD_FILE_TYPE == 3:
+        return os.path.exists(_output_path+str(comic_id)+".zip")
+    elif UPLOAD_FILE_TYPE == 4:
+        return os.path.exists(_output_path+str(comic_id)+"_encrypted.zip")
+
+async def do_upload(bot, ev, comic_id):
+    '''上传文件到'''
+    if UPLOAD_FILE_TYPE == 1:
+        file_path = _output_path+str(comic_id)+".pdf"
+    elif UPLOAD_FILE_TYPE == 2:
+        file_path = _output_path+str(comic_id)+"_encrypted.pdf"
+    elif UPLOAD_FILE_TYPE == 3:
+        file_path = _output_path+str(comic_id)+".zip"
+    elif UPLOAD_FILE_TYPE == 4:
+        file_path = _output_path+str(comic_id)+"_encrypted.zip"
+    try:
+        path = await llob_cross_host_download_file(bot, path2b64(file_path))
+        await bot.upload_group_file(group_id=ev.group_id, file=path, name=os.path.basename(file_path))
+    except Exception as e:
+        await bot.send(ev, f"上传群文件失败！\n {e}")
+
+async def process_file(comic_id):
+    '''根据UPLOAD_FILE_TYPE 处理文件
+    Args:
+        comic_id: str jmid
+    Returns:
+        bool: True表示处理成功，False表示处理失败
+        str: 处理成功时返回空字符串，处理失败时返回错误信息
+    '''
+    pure_pdf = _output_path+str(comic_id)+".pdf"
+    enc_pdf = _output_path+str(comic_id)+"_encrypted.pdf"
+    enc_zip_n_pure_pdf = _output_path+str(comic_id)+".zip"
+    enc_zip_n_enc_pdf = _output_path+str(comic_id)+"_encrypted.zip"
+    pwd = reverse_string(str(comic_id))
+
+    if UPLOAD_FILE_TYPE == 1: 
+        return True, ""
+    if UPLOAD_FILE_TYPE in [2, 4]:
+        if not (await encrypt_pdf(pure_pdf, enc_pdf, pwd)):
+            msg = f"{comic_id}加密PDF文件失败"
+            return False, msg
+        if UPLOAD_FILE_TYPE == 2:
+            return True, ""
+        elif UPLOAD_FILE_TYPE == 4:
+            if not (await encrypt_zip(enc_pdf, enc_zip_n_enc_pdf, pwd)):
+                msg = f"{comic_id}加密zip文件失败(with encrypted pdf)"
+                return False, msg
+            return True, ""
+    if UPLOAD_FILE_TYPE == 3:
+        if not (await encrypt_zip(pure_pdf, enc_zip_n_pure_pdf, pwd)):
+            msg = f"{comic_id}加密zip文件失败(with pure pdf)"
+            return False, msg
+        return True, ""
+        
 @sv.on_rex(r'^jm(\d+)$')
 async def jmcomic_download(bot, ev):
     comic_id = int(ev.match.group(1))
     if await is_exist_comic(comic_id):
         await bot.send(ev, f"JMComic: {comic_id}已存在，正在上传...")
-        try:
-            path = await llob_cross_host_download_file(bot, path2b64(_pdf_path+str(comic_id)+"_encrypted.pdf"))
-            await bot.upload_group_file(group_id=ev.group_id, file=path, name=str(comic_id)+".pdf")
-        except Exception as e:
-            await bot.send(ev, f"上传群文件失败！\n {e}")
-        finally:
-            return
+        await do_upload(bot, ev, comic_id)
+        return
+
     await bot.send(ev, f"正在下载JMComic: {comic_id}，请稍候...")
     download_res = await download_comic(comic_id)  # [1]是漫画名称
     if not (download_res[0]):
         await bot.send(ev, "Failed to download comic.")
         return
-    if not (await encrypt_pdf(_pdf_path+str(comic_id)+".pdf", _pdf_path+str(comic_id)+"_encrypted.pdf", reverse_string(str(comic_id)))):
+    # 开始做加密
+    ok, msg = await process_file(comic_id)
+    if not (ok):
+        await bot.send(ev, f"加密失败！\n {msg}")
         return
     await bot.send(ev, f"uploading...")
-    try:
-        path = await llob_cross_host_download_file(bot, path2b64(_pdf_path+str(comic_id)+"_encrypted.pdf"))
-        await bot.upload_group_file(group_id=ev.group_id, file=path, name=str(comic_id)+".pdf")
-    except Exception as e:
-        await bot.send(ev, f"上传群文件失败！\n {e}")
+    await do_upload(bot, ev, comic_id)
 
 # async def main():
 #     comic_id = 529481
